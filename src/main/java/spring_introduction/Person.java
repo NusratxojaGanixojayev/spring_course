370c61bde30b2a49d85f1bb3a2d56e7eaa4ba955
NusratxojaGanixojayev <ganixojayev91@gmail.com>
2021-07-04 12:13:50 +0500

    009 dars

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -2,6 +2,9 @@ package spring_introduction;
 
 public class Person {
     private Pet pet;
+    private String Surname;
+    private int age;
+
 
     public Person() {
         System.out.println("Person");
@@ -11,6 +14,22 @@ public class Person {
     public void setPet(Pet pet) {
         System.out.println(" Class person set");
         this.pet = pet;
+    }
+
+    public String getSurname() {
+        return Surname;
+    }
+
+    public void setSurname(String surname) {
+        Surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public void callYourPet(){
--- src/main/java/spring_introduction/Test3.java
+++ src/main/java/spring_introduction/Test3.java
@@ -5,11 +5,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test3 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
-                new ClassPathXmlApplicationContext("applicationContext.xml");
+                new ClassPathXmlApplicationContext("applicationCon.xml");
 
 //        Pet pet = context.getBean("myPet", Pet.class);
 //        Pet pet = new Dog();
-        Person person = context.getBean("myPet", Person.class);
+        Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
 
         context.close();

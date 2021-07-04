4b49a6cb266f4f3363e9bc64ea3e55f2392183e5
NusratxojaGanixojayev <ganixojayev91@gmail.com>
2021-07-04 11:47:57 +0500

    007 darsga keldim.YArmiga songra

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -3,7 +3,13 @@ package spring_introduction;
 public class Person {
     private Pet pet;
 
-    public Person(Pet pet) {
+    public Person() {
+        System.out.println("Person");
+
+    }
+
+    public void setPet(Pet pet) {
+        System.out.println(" Class person set");
         this.pet = pet;
     }
 
